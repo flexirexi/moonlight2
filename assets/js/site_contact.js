79269fbb8f6d14e525adb8944e5f0dea02419a27
flexirexi <felix_lehmann@ymail.com>
2025-06-07 22:34:51 +0200

    feat: add bilingual contact form and email template

--- assets/js/site_contact.js
+++ assets/js/site_contact.js
@@ -4,13 +4,23 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
     emailjs.init("td8yqeDgGLiud1lKu"); // kein secret key, nur public
 
     const form = this;
-
-    emailjs.sendForm("service_5x00awe", "template_am9lqdr", form)
-        .then(function () {
-            alert("Nachricht erfolgreich gesendet! Wir melden uns bald.");
-            form.reset();
-        }, function (error) {
-            console.error("Email konnte nicht gesendet werden:", error);
-            alert("Ein Fehler ist aufgetreten. Bitte versuche es später erneut.");
-        });
+    if (document.documentElement.lang == "de") {
+        emailjs.sendForm("service_5x00awe", "template_am9lqdr", form)
+            .then(function () {
+                alert("Nachricht erfolgreich gesendet! Wir melden uns bald.");
+                form.reset();
+            }, function (error) {
+                console.error("Email konnte nicht gesendet werden:", error);
+                alert("Ein Fehler ist aufgetreten. Bitte versuche es später erneut.");
+            });
+    } else {
+        emailjs.sendForm("service_5x00awe", "template_mb339u7", form)
+            .then(function () {
+                alert("Message sent! We'll come back to you soon.");
+                form.reset();
+            }, function (error) {
+                console.error("Email konnte nicht gesendet werden:", error);
+                alert("An error occured. Please try it again.");
+            });
+    }
 });
